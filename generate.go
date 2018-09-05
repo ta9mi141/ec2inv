ff50e27bcb350b235a6dae17c23f9c926c48f0ca
it_akumi <it.akumi1011@gmail.com>
2018-09-05 17:30:37 +0900

    Add uploadTemplateToS3
    Use a template in S3 bucket. If you use the template specified by
    TemplateBody parameter, a maximum length of the file is 51200 bytes.
    On the other hand, if you specify the location of the template by
    TemplateURL parameter, its max size is 460800 bytes.

--- generate.go
+++ generate.go
@@ -1,10 +1,55 @@
 package main
 
 import (
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"log"
+	"os"
 )
 
-func createAnsibleTargets(stackName string) error {
+func uploadTemplateToS3(templatePath string) (string, error) {
+	bucket, ok := os.LookupEnv("S3_BUCKET_NAME")
+	if !ok {
+		return "", errors.New("You must set Environment Variable 'S3_BUCKET_NAME'")
+	}
+	key, ok := os.LookupEnv("AWS_ACCESS_KEY")
+	if !ok {
+		return "", errors.New("You must set Environment Variable 'AWS_ACCESS_KEY'")
+	}
+
+	template, err := os.Open(templatePath)
+	if err != nil {
+		return "", err
+	}
+	defer template.Close()
+
+	return bucket + key, nil
+}
+
+func createAnsibleTargets(stackName, templateURL *string) error {
+	client := cloudformation.New(
+		session.New(aws.NewConfig().WithRegion("ap-northeast-1")), nil,
+	)
+
+	_, err := client.CreateStack(
+		&cloudformation.CreateStackInput{
+			StackName:   stackName,
+			TemplateURL: templateURL,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	err = client.WaitUntilStackCreateComplete(
+		&cloudformation.DescribeStacksInput{StackName: stackName},
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -14,10 +59,17 @@ func printInventory(stackName string) error {
 
 func main() {
 	const (
-		stackName = "AnsibleTargets"
+		stackName    = "AnsibleTargets"
+		templatePath = "./ec2.yml"
 	)
 
-	if err := createAnsibleTargets(stackName); err != nil {
+	templateURL, err := uploadTemplateToS3(templatePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = createAnsibleTargets(aws.String(stackName), aws.String(templateURL))
+	if err != nil {
 		log.Fatal(err)
 	}
 
